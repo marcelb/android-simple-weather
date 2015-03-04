bdcfaba132d7fc8a6a659cee64a8001410ee8c0e
Marcel Bankmann <marcel.bankmann@devbliss.com>
2015-03-04 15:37:46 +0100

    add map location intent

--- app/src/main/java/com/devbliss/sunshine/app/MainActivity.java
+++ app/src/main/java/com/devbliss/sunshine/app/MainActivity.java
@@ -1,10 +1,15 @@
 package com.devbliss.sunshine.app;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.net.URI;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -42,7 +47,27 @@ public class MainActivity extends ActionBarActivity {
             return true;
         }
 
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_show_location) {
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+//            Uri.Builder builder = new Uri.Builder();
+//            builder.encodedPath("geo:0,0");
+//            builder.appendQueryParameter("q",prefs.getString(getString(R.string.pref_location_key),getString(R.string.pref_location_default)));
+//            showMap(builder.build());
+            showMap(Uri.parse("geo:0,0?q="+prefs.getString(getString(R.string.pref_location_key),getString(R.string.pref_location_default))));
+            return true;
+        }
+
+
         return super.onOptionsItemSelected(item);
+    }
+
+    private void showMap(Uri geoLocation) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 
 }
